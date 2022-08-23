cf857c315500a59bf5830c56f436c3d05e6b0bdf
Arturo Chari <arturofelixchari@gmail.com>
2022-08-23 19:31:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,18 @@ from helpers import is_valid_text
 from schemas import Input, Result
 from services import closed_strokes_count
 
-app = FastAPI()
+app = FastAPI(
+    title='Menta Tickets Challenge!',
+    description='Closed Strokes Counter API',
+    version='1.0.0',
+    openapi_tags=[{
+        "name": "default",
+        "description": "Given a text return the count of closed strokes. Font-Text: DejaVuSans"
+    }]
+)
 
 @app.post("/", response_model=Result)
-def root(input: Input):
+def closed_strokes(input: Input):
     if not is_valid_text(input.text):
         raise HTTPException(status_code=400, detail='Not allowed chacarters into text')
     
