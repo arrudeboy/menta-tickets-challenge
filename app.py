d877b04da15dd59d8969afae47a1fa1ae1a0d855
Arturo Chari <arturofelixchari@gmail.com>
2022-08-23 19:48:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 
 from helpers import is_valid_text
-from schemas import Input, Result
+from schemas import Input, Output
 from services import closed_strokes_count
 
 app = FastAPI(
@@ -14,10 +14,10 @@ app = FastAPI(
     }]
 )
 
-@app.post("/", response_model=Result)
+@app.post("/", response_model=Output)
 def closed_strokes(input: Input):
     if not is_valid_text(input.text):
         raise HTTPException(status_code=400, detail='Not allowed symbols into text. Allowed symbols are: @ # $ % ∞ ‰ & / ( ) = ? ¿ _ -')
     
     count = closed_strokes_count(input.text)
-    return Result(closed_strokes_count=count)
+    return Output(closed_strokes_count=count)
